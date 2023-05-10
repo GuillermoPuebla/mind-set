eaf260639e1809df074c84d803eafb8f60c1189a
Don Yin <Don_Yin@outlook.com>
2023-05-10 12:04:35 +0100

    various minor format changes
    Former-commit-id: 304c5c706ed5e89c38f76ca05515c559fadf6064

--- src/stimuli/src/utils/shapes.py
+++ src/stimuli/src/utils/shapes.py
@@ -1,3 +1,9 @@
+import cv2
+import numpy as np
+import os
+from pathlib import Path
+
+
 class SizeMonitor:
     def __init__(self, window=3):
         self.window = window
@@ -8,7 +14,10 @@ class SizeMonitor:
 
     def check_stable(self):
         # if the last n are the same and different from the initial number
-        return len(set(self.sizes[-self.window :])) == 1 and self.sizes[-1] != self.sizes[0]
+        return (
+            len(set(self.sizes[-self.window :])) == 1
+            and self.sizes[-1] != self.sizes[0]
+        )
 
 
 def chaikins_corner_cutting(points, num_iterations):
@@ -24,3 +33,37 @@ def chaikins_corner_cutting(points, num_iterations):
             new_points.extend([q, r])
         points = new_points
     return points
+
+
+def silhouettes_to_outlines(file_path, width=1):
+    """
+    Converting images of silhouettes to outlines
+    """
+    image = cv2.imread(str(file_path), cv2.IMREAD_GRAYSCALE)
+
+    # _, image = cv2.threshold(image, 128, 255, cv2.THRESH_BINARY_INV)
+    blurred = cv2.GaussianBlur(image, (3, 3), 0)
+    edges = cv2.Canny(blurred, 100, 200)
+
+    # Dilate the edges to create the outline with the given width
+    kernel = np.ones((width, width), np.uint8)
+    outline_image = cv2.dilate(edges, kernel)
+
+    return outline_image
+
+
+if __name__ == "__main__":
+    silhouettes_path = Path("private", "baker-1", "Silhouettes")
+    silhouettes_files = os.listdir(silhouettes_path)
+    silhouettes_files = [i for i in silhouettes_files if not i.startswith(".")]
+
+    # process_files(silhouettes_files, silhouettes_path, outline_width=2)
+    output_path = Path("temp")
+    output_path.mkdir(exist_ok=True)
+
+    for file in silhouettes_files:
+        file_path = silhouettes_path / file
+        outline_image = silhouettes_to_outlines(file_path, width=2)
+
+        output_file_path = output_path / file
+        cv2.imwrite(str(output_file_path), outline_image)
