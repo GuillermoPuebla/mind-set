b25c0875b1e8af7c85565c4609a66ec639873073
ValerioB88 <valerio.biscione@gmail.com>
2023-09-13 09:51:13 +0100

    Add linedrawings example for imagenet_classification
    Former-commit-id: 06bd3b0e7e61b79662e0e988f16537ea802b7dd3

---
+++ src/utils/imagenet_classification/example/linedrawings_eval.py
@@ -0,0 +1,36 @@
+import os
+import pandas as pd
+import toml
+from src.utils.dataset_utils import ImageNetClasses
+from src.utils.decoder.examples.regression.jastrow_illusion.jastrow_utils import (
+    adjust_csv,
+)
+from src.utils.imagenet_classification.eval import classification_evaluate
+import pathlib
+
+
+dataset_folder = pathlib.Path("datasets_examples") / "linedrawings"
+
+# We create a small Jastrow_Illusion dataset just for this example.
+from src.generate_datasets.gestalt.linedrawings.generate_dataset import (
+    generate_all as linedrawings_generate,
+)
+
+if not pathlib.Path(dataset_folder).exists():
+    linedrawings_generate(
+        output_folder=dataset_folder,
+    )
+
+# We need to add the ImageNetClassIndex column, based on the "Class" column.
+add_class = ImageNetClasses()
+add_class.add_to_annotation_file_path(
+    str(dataset_folder / "annotation.csv"),
+    "Class",
+    str(dataset_folder / "annotation_w_imagenet_idxs.csv"),
+)
+
+with open(os.path.dirname(__file__) + "/linedrawings_config.toml", "r") as f:
+    toml_config = toml.load(f)
+classification_evaluate(**toml_config)
+
+##
