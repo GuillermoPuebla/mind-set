c537394721a3147408f55c57587247a2d34a2bb1
Don Yin <Don_Yin@outlook.com>
2023-09-15 15:40:05 +0100

    update workflow
    Former-commit-id: b0c419a6635c37a2e86eca5d29491fad3a1cef47

--- _test.py
+++ _test.py
@@ -1,16 +1,16 @@
 """
 Script for regression test / revisit doc string later
 """
-import sys
 from src.utils.generate_default_pars_toml_file import create_config
 from src.generate_datasets_from_toml import generate_toml
 from pathlib import Path
 import toml
 import shutil
+import os
 from tqdm import tqdm
-import multiprocessing as mp
+from collections import defaultdict
 import base64
-import nbformat as nbf
+from nbformat import v4 as nbf
 
 
 def encode_image_base64(image_path):
@@ -19,28 +19,51 @@ def encode_image_base64(image_path):
     return encoded_bytes.decode("utf-8")
 
 
-class CountImagesIn:
-    def __init__(self, path, generate_thread):
-        self.limit = 10
-        self.path = path
-        self.generate_thread = generate_thread
-        self.count = self()
-        self.progress_bar = tqdm(total=self.limit)
-        self.update_loop()
+def build_dataset_structure(path, dataset_structure):
+    # collect first 9
+    for root, dirs, files in sorted(os.walk(path)):
+        image_files = [
+            Path(root) / file for file in sorted(files) if file.endswith(".png")
+        ][:9]
+        if image_files:
+            d = dataset_structure
+            path_parts = Path(root).relative_to(path).parts
+            for part in path_parts:
+                d = d.setdefault(part, defaultdict(dict))
+            d["images"] = image_files
 
-    def __call__(self):
-        self.count = sum(1 for _ in self.path.rglob("*.png"))
 
-    def update_loop(self):
-        while True:
-            self()
-            self.progress_bar.update(self.count - self.progress_bar.n)
-            if self.count >= self.limit:
-                self.generate_thread.terminate()
-                break
+def generate_headers(nb, data, level=1):
+    for key, value in sorted(data.items()):
+        if key != "images":
+            nb.cells.append(nbf.new_markdown_cell(f"{'#' * level} {key}"))
+            generate_headers(nb, value, level + 1)
+        else:
+            nb.cells.append(nbf.new_markdown_cell(create_table_markdown(value)))
+
+
+def create_table_markdown(images):
+    table_markdown = "<table><tr>"
+    for index, image_path in enumerate(images):
+        base64_data = encode_image_base64(image_path)
+        if base64_data:
+            table_markdown += f"<td><img src='data:image/png;base64,{base64_data}' alt='{image_path.name}'></td>"
+            if (index + 1) % 3 == 0 or (index + 1) == len(images):
+                table_markdown += "</tr><tr>"
+    table_markdown += "</tr></table>"
+    return table_markdown
+
+
+def generate_notebook(dataset_structure):
+    save_to = Path("tests", "samples_preview.ipynb")
+    nb = nbf.new_notebook()
+    generate_headers(nb, dataset_structure)
+    with open(save_to, "w") as f:
+        f.write(nbf.writes(nb))
 
 
 def test_generate_toml():
+    # -------- folder preparation --------
     toml_save_to = Path("tests", "all_datasets.toml")
     data_save_to = Path("tests", "sample_data")
     toml_individual_save_to = Path("tests", "tomls")
@@ -48,6 +71,7 @@ def test_generate_toml():
     if data_save_to.exists():
         shutil.rmtree(data_save_to)
 
+    # -------- making config --------
     create_config(save_to=toml_save_to)
 
     with open(toml_save_to, "r") as f:
@@ -60,66 +84,23 @@ def test_generate_toml():
         dataset_category_name = Path(key).parent.parent.name
         output_folder = data_save_to / dataset_category_name / dataset_name
         toml_config[key].update({"output_folder": output_folder.as_posix()})
-
         name_path_dict.update({dataset_name: output_folder})
+
+        # -------- set num_samples to 10 --------
+        for sub_key in toml_config[key]:
+            if "num_samples" in sub_key:
+                toml_config[key][sub_key] = 9
 
         with open(toml_individual_save_to / f"{dataset_name}.toml", "w") as f:
             toml.dump({key: toml_config[key]}, f)
 
     # -------- generate each dataset --------
-    for key in name_path_dict:
-        toml_path = toml_individual_save_to / f"{key}.toml"
-        output_path = name_path_dict[key]
-
-        process_generate = mp.Process(target=generate_toml, args=(toml_path,))
-        process_generate.start()
-        CountImagesIn(output_path, generate_thread=process_generate)
+    [generate_toml(toml_individual_save_to / f"{key}.toml") for key in name_path_dict]
 
     # -------- collect generated images --------
-    dataset_structure = {}
-    for key in toml_config:
-        dataset_name = Path(key).parent.name
-        dataset_category_name = Path(key).parent.parent.name
-        output_folder = data_save_to / dataset_category_name / dataset_name
-        images = list(output_folder.rglob("*.png"))
-
-        if dataset_category_name not in dataset_structure:
-            dataset_structure[dataset_category_name] = {}
-        if dataset_name not in dataset_structure[dataset_category_name]:
-            dataset_structure[dataset_category_name][dataset_name] = []
-        dataset_structure[dataset_category_name][dataset_name].extend(images)
-
-    nb = nbf.v4.new_notebook()
-
-    for category_name, datasets in dataset_structure.items():
-        nb.cells.append(nbf.v4.new_markdown_cell(f"# {category_name}"))
-
-        for dataset_name, images in datasets.items():
-            nb.cells.append(nbf.v4.new_markdown_cell(f"## {dataset_name}"))
-
-            # Initialize table markdown
-            table_markdown = "<table><tr>"
-
-            for index, image_path in enumerate(images):
-                base64_data = encode_image_base64(image_path)
-
-                if len(base64_data):  # filter empty
-                    table_markdown += f"<td><img src='data:image/png;base64,{base64_data}' alt='{image_path.name}'></td>"
-                    # check last
-                    if (index + 1) % 3 == 0 or (index + 1) == len(images):
-                        table_markdown += "</tr><tr>"
-
-            # close table
-            table_markdown += "</tr></table>"
-            nb.cells.append(nbf.v4.new_markdown_cell(table_markdown))
-
-    with open("GeneratedNotebook.ipynb", "w") as f:
-        nbf.write(nb, f)
-
-    # now terminate the current process manually as the multiprocessing seems to 
-    # do some weird stuff with
-    # semaphores and locks
-    sys.exit()
+    dataset_structure = defaultdict(dict)
+    build_dataset_structure(data_save_to, dataset_structure)
+    generate_notebook(dataset_structure)
 
 
 if __name__ == "__main__":
